39cfb42ed0c2cd4a3adeae4a029924364571faae
Mukul <mukulkundu22@gmail.com>
2025-07-05 22:30:29 +0530

    .

--- src/CodeModules/GenericListView.tsx
+++ src/CodeModules/GenericListView.tsx
@@ -6,8 +6,19 @@ export default function GenericListView(){
 
     return(
 
-        <>
-        
-        </>
+        <div className="flex flex-col items-center justify-center">
+        <div>
+            [Showing Results]
+            [Option Btn]
+        </div>
+
+        <div>
+            Grid
+        </div>
+
+        <div>
+            [Pagenation]
+        </div>
+        </div>
     )
 }
--- src/Components/GenericListViewPage.tsx
+++ src/Components/GenericListViewPage.tsx
@@ -1,3 +1,4 @@
+import GenericListView from "../CodeModules/GenericListView";
 
 
 export default function GenericListViewPage(){
@@ -7,7 +8,19 @@ export default function GenericListViewPage(){
     return(
 
         <>
-        <div></div>
+        <div className="pt-14 ml-24 dark:text-white">
+                    <h1 className="text-4xl font-medium">Generic List View</h1>
+        
+                    <article className="mt-2 text-sm text-gray-700">
+                        A reusable data grid component with pagination, debounced search, and dynamic column visibility.
+                    </article>
+        
+                    <section className="mt-12 bg-gray-200 dark:bg-[#1d1c1c] h-50 rounded-lg flex items-center justify-center w-[50%]">
+                        <GenericListView/>
+                    </section>
+        
+        
+                </div>
         </>
     )
 }
